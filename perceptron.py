d64efa5774302b22395e182137d5a68996681f1f
noobber <18640147180@163.com>
2019-01-01 21:00:03 +0800

    感知器函数和and真值表的训练实例

---
+++ perceptron.py
@@ -0,0 +1,112 @@
+# python3中，reduce()函数已经在全局名字空间中被移除
+from functools import reduce
+
+
+class Perceptron(object):
+    def __init__(self, input_num, activator):
+        '''
+        初始化感知器，设置输入参数的个数，以及激活函数
+        激活函数的类型为double->double
+        '''
+        self.activator = activator
+        # 权重向量初始化为0
+        self.weights = [0.0 for _ in range(input_num)]
+        # 偏置项初始化为0
+        self.bias = 0.0
+
+    def __str__(self):
+        '''
+        打印学习到的权重，偏置项
+        '''
+        return 'weights\t:%s\n bias:%f\n' % (list(self.weights), self.bias)
+
+    def predict(self, input_vec):
+        '''
+        输入向量，输出感知器的计算结果
+        '''
+        # 把input_vec[x1,x2,x3...]和weights[w1,w2,w3]打包在一起
+        # 变成[(x1, w1),(x2,w2),(x3,w3),...]
+        # 然后利用map函数计算[x1*w1,x2*w2，x3*w3]  !!!
+        # 最后利用reduce求和
+        return self.activator(
+            reduce(lambda a, b: a+b,
+                   map(lambda x, w: x*w, input_vec, self.weights), 0.0
+                   )+self.bias)
+
+    def train(self, input_vecs, labels, iteration, rate):
+        '''
+        输入训练数据：一组向量，与每个向量对应的label；以及训练轮数，学习率
+        '''
+        for i in range(iteration):
+            self.one_iteration(input_vecs, labels, rate)
+
+    def one_iteration(self, input_vecs, labels, rate):
+        '''
+        一次迭代，把所有的训练数据过一遍
+        '''
+        # 把输入和输出打包在一起，成为样本的列表[(inputVec,label),....]
+        # 而每个训练样本是(inputVec,label)
+        samples = zip(input_vecs, labels)
+        # 对每个样本，按照探至七规则更新权重
+        for (input_vec, label) in samples:
+            # 计算感知器在当前权重下的输出
+            output = self.predict(input_vec)
+            # 更新权重
+            self.update_weights(input_vec, output, label, rate)
+
+    def update_weights(self, input_vec, output, label, rate):
+        '''
+        按照感知器规则更新权重
+        '''
+        # 把input_vec[x1,x2,x3,...]和weights[w1,w2,w3,...]打包
+        # 然后利用感知器规则更新权重
+        delta = label - output  # 误差
+        # map函数返回的仅是map对象，必须列表化才行！！！找错误找着找着就秃了0.0，终于搞出来了
+        self.weights = list(map(
+            lambda x, w: w+rate*delta*x, input_vec, self.weights
+        ))
+        self.bias += rate*delta
+
+
+def f(x):
+    '''
+    定义激活函数f
+    '''
+    return 1 if x > 0 else 0
+
+
+def get_training_dataset():
+    '''
+    基于and真值表构建训练数据
+    '''
+    # 构建训练数据
+    # 输入向量列表
+    input_vecs = [[1, 1], [0, 0], [1, 0], [0, 1]]
+    # 期望的输出列表
+    labels = [1, 0, 0, 0]
+    return input_vecs, labels
+
+
+def train_and_perceptron():
+    '''
+    使用and真值表训练感知器
+    '''
+    # 创建感知器，输入参数个数为2,激活函数为f
+    p = Perceptron(2, f)
+    # 训练，迭代10轮，学习速率为0.1
+    input_vecs, labels = get_training_dataset()
+    p.train(input_vecs, labels, 10, 0.1)
+    # 返回训练好的感知器
+    return p
+
+
+if __name__ == '__main__':
+    # 训练and感知器
+    and_perception = train_and_perceptron()
+    # 打印训练获得的权重,偏置项
+    print(and_perception)
+    # 测试
+    print('1 and 1 = %d' % and_perception.predict([1, 1]))
+    print('0 and 0 = %d' % and_perception.predict([0, 0]))
+    print('1 and 0 = %d' % and_perception.predict([1, 0]))
+    print('0 and 1 = %d' % and_perception.predict([0, 1]))
